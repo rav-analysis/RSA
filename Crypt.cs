5a7a5250978aae99f3b4f0d6d5c204b480b60f8e
rav-analysis <bakir_hadzic@outlook.com>
2021-10-07 01:07:18 +0200

    Add project files.

---
+++ Crypt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSA
+{
+    public class Crypt
+    {
+        public byte[] AsymmetricEncrypt(byte[] data)
+        {
+            return _rsa.Encrypt(data, false);
+        }
+        private readonly RSACryptoServiceProvider _rsa = new();
+
+        public void SetPublicKey(string publicKeyFile)
+        {
+            var publicKey = File.ReadAllText(publicKeyFile);
+            _rsa.ImportFromPem(publicKey);
+        }
+        public byte[] AsymmetricDecrypt(byte[] data)
+        {
+            return _rsa.Decrypt(data, false);
+        }
+
+        public void SetPrivateKey(string privateKeyFile)
+        {
+            var privateKey = File.ReadAllText(privateKeyFile);
+            _rsa.ImportFromPem(privateKey);
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace RSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = args[0];
+            var crypt = new Crypt();
+            if (operation == "encrypt")
+            {
+                var publicKeyFile = args[1];
+                crypt.SetPublicKey(publicKeyFile);
+
+                var data = Encoding.UTF8.GetBytes(Console.In.ReadToEnd());
+                var encryptedData = crypt.AsymmetricEncrypt(data);
+                Console.WriteLine(System.Convert.ToBase64String(encryptedData));
+            }
+            else if (operation == "decrypt")
+            {
+                var privateKeyFile = args[1];
+                crypt.SetPrivateKey(privateKeyFile);
+                var encryptedData = System.Convert.FromBase64String(Console.In.ReadToEnd());
+                var decryptedData = crypt.AsymmetricDecrypt(encryptedData);
+                Console.WriteLine(Encoding.UTF8.GetString(decryptedData));
+            }
+        }
+    }
+}
